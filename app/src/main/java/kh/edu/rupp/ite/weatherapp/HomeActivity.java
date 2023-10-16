c00a565eb26fced7e1f5461cb6cee66cf7c1435e
bunchhean.lu <53104062+mageewhy@users.noreply.github.com>
2023-10-16 13:57:12 +0700

    test commit

--- app/src/main/java/kh/edu/rupp/ite/weatherapp/HomeActivity.java
+++ app/src/main/java/kh/edu/rupp/ite/weatherapp/HomeActivity.java
@@ -1,10 +1,10 @@
 package kh.edu.rupp.ite.weatherapp;
 
+
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -30,7 +30,6 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = binding.toolbar;
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-        toolBarLayout.setTitle(getTitle());
 
     }
 }
