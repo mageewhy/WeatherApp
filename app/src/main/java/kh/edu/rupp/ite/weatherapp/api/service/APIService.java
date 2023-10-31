f893e0666b394f179c4d7d16c16998ee54857b71
sBothrasith <somouthbothrasith1@gmail.com>
2023-10-31 23:05:17 +0700

    weather model

---
+++ app/src/main/java/kh/edu/rupp/ite/weatherapp/api/service/APIService.java
@@ -0,0 +1,11 @@
+package kh.edu.rupp.ite.weatherapp.api.service;
+
+import kh.edu.rupp.ite.weatherapp.api.model.Weather;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface APIService {
+
+    @GET("/v1/forecast.json?key=25714936cd1e401ea1270735231610&q=Phnom%20Penh")
+    Call<Weather> LoadWeather();
+}
--- app/src/main/java/kh/edu/rupp/ite/weatherapp/ui/fragment/HomeFragment.java
+++ app/src/main/java/kh/edu/rupp/ite/weatherapp/ui/fragment/HomeFragment.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import kh.edu.rupp.ite.weatherapp.api.model.Weather;
+import kh.edu.rupp.ite.weatherapp.api.service.APIService;
 import kh.edu.rupp.ite.weatherapp.databinding.FragmentHomeBinding;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
@@ -25,5 +33,39 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        LoadWeatherFromServer();
     }
+
+    private void LoadWeatherFromServer() {
+        Retrofit httpClient = new Retrofit.Builder()
+                .baseUrl("https://api.weatherapi.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        APIService apiService = httpClient.create(APIService.class);
+
+        Call<Weather> weather = apiService.LoadWeather();
+
+        weather.enqueue(new Callback<Weather>() {
+            @Override
+            public void onResponse(Call<Weather> call, Response<Weather> response) {
+                if(response.isSuccessful()){
+                    ShowWeather(response.body());
+                }else{
+                    Toast.makeText(getContext(), "Received Failed" , Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Weather> call, Throwable t) {
+                Toast.makeText(getContext(), "Received failed" + t, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void ShowWeather(Weather weather) {
+
+    }
+
 }
